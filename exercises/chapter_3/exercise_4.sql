c180a2de3a26a8704f00d58f7a7473eb86e6dbac
Zhu Zhanyan <program.nom@gmail.com>
2021-07-16 11:13:09 +0800

    doc(exercises): Document reasoning behind Exercise 3.4's solution

--- exercises/chapter_3/exercise_4.sql
+++ exercises/chapter_3/exercise_4.sql
@@ -11,4 +11,8 @@ FROM
 WHERE
   date(r.rental_date) = '2005-06-14'
 ORDER BY
+  -- inspecting the result set we can make 2 observations:
+  -- 1. email column does not seem to be in osrted order
+  -- 2. return date is in reverse / descending sorted order
+  -- hence we can assume that the result set is in reverse sorted order.
   date(r.return_date) DESC;
